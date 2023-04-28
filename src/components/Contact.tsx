e575740d7ae087ebb60a3f31dc59c2a9cc9e1678
Juan Cruz Francitorra <jufovich@gmail.com>
2023-04-28 15:21:15 -0300

    email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,18 +7,18 @@ const Contact = () => {
   return (
     <>
     <div>
-      <form className="centrar">
+      <form className="centrar" action="https://formsubmit.co/juanfrancitorra@gmail.com" method="POST">
         <div className="form-group">
     <h1>Contact</h1>
           <input placeholder="Your Name" type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
           <i></i>
         </div>
         <div className="form-group">
-          <input placeholder="Your Email" type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input placeholder="Your Email" type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
           <i></i>
         </div>
         <div className="form-group">
-          <textarea placeholder="Your Message" id="message" value={message} onChange={(e) => setMessage(e.target.value)}/>
+          <textarea placeholder="Your Message" id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)}/>
           <i></i>
         </div>
         <button type="submit">Send</button>
